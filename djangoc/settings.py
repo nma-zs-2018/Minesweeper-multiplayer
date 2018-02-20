be9246994622c92e50f601549158ddb47b4d4883
Karolis Vycius <k.vycius@gmail.com>
2018-02-20 12:40:41 +0200

    allow all hosts

--- djangoc/settings.py
+++ djangoc/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '#@s#qr82$$rd6*c*0(%)b-d3^jbwwval3c_uh9po%ck%%=8k#&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['159.89.102.219', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
